ffc4f9a108dba6425b2ad97cf8021fef9843ac86
Vasanti Dhankate <dr.dvasanti@gmail.com>
2026-03-11 00:28:52 +0530

    App.js updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import Exam from "./pages/Exam";
+import AddQuestion from "./pages/AddQuestion";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <Router>
+      <div style={{ background: "#F8F9FA", minHeight: "100vh", padding: "20px" }}>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/exam" element={<Exam />} />
+          <Route path="/add-question" element={<AddQuestion />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
