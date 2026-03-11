1ecfdde4fe5281e90afc99baa5216dfe75b739f2
Vasanti Dhankate <dr.dvasanti@gmail.com>
2026-03-11 09:07:45 +0530

    commented code

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard";
-import Exam from "./pages/Exam";
-import AddQuestion from "./pages/AddQuestion";
+//import Register from "./pages/Register";
+//import Dashboard from "./pages/Dashboard";
+//import Exam from "./pages/Exam";
+// import AddQuestion from "./pages/AddQuestion";
 
 function App() {
   return (
     <Router>
       <div style={{ background: "#F8F9FA", minHeight: "100vh", padding: "20px" }}>
         <Routes>
-          <Route path="/" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/exam" element={<Exam />} />
-          <Route path="/add-question" element={<AddQuestion />} />
+          <Route path="/" element={<div>Register</div> } />
+          <Route path="/dashboard" element={<div>Register</div>} />
+          <Route path="/exam" element={<div>Register</div>} />
+          <Route path="/add-question" element={<div>Register</div>} />
         </Routes>
       </div>
     </Router>
