5c1de33e6f131d3d39a9ef02eefa341fb2657590
Sonali Kanthe <sonalikanthe33@gmail.com>
2026-03-12 16:51:49 +0530

    Add AddQuestion page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginForm from "./pages/LoginForm";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AddQuestion from "./pages/AddQuestion";
+import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <div style={{ background: "#F8F9FA", minHeight: "100vh", padding: "20px" }}>
-        <Routes>
-          {/* Render the actual LoginForm component here */}
-          <Route path="/login" element={<LoginForm />} />  
-          
-          {/* Optional: Add a default route */}
-          <Route path="/" element={<h1>Welcome to Online Exam System</h1>} />
-        </Routes>
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AddQuestion />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
