cfd6506084c161d168de1282cbb6dc41ebdd4b67
akanksha padule <paduleakanksha@gmail.com>
2026-03-13 14:24:08 +0530

    Added Navbar and Dashboard page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AddQuestion from "./pages/AddQuestion";
 import "./App.css";
+import Dashboard from "./pages/Dashboard";
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<AddQuestion />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
