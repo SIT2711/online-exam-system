596d09429b46c5137acc81d93fb16e70726b42ee
student ofYashoda <jagtapsatyajit14@gmail.com>
2026-03-26 10:07:06 +0530

    Update changes

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -27,7 +27,7 @@ function Register() {
     return;
   }
 
-  fetch("http://localhost/online-exam-api/auth/register.php", {
+  fetch("http://localhost/online-exam-system/auth/register.php", {
     method: "POST",
     headers: {
       "Content-Type": "application/x-www-form-urlencoded"
