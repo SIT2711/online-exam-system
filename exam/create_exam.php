b8c7fc7213422df40e897c247903df76642d6d64
Janhavi Bondge <janhvibondge@gmail.com>
2026-04-04 10:55:39 +0530

    update create_exam.php

--- exam/create_exam.php
+++ exam/create_exam.php
@@ -22,15 +22,15 @@ $teacher_id = $data['teacher_id'];
 $exam_title = $data['exam_title'];
 $subject = $data['subject'];
 $duration = $data['duration'];
-$total_Questions = $data['total_Questions'];
+$total_marks = $data['total_marks'];
 $start_date = $data['start_date'];
 $end_date = $data['end_date'];
 
 
 $sql = "INSERT INTO exams 
-(teacher_id, exam_title, subject, duration, total_Questions, start_date, end_date)
+(teacher_id, exam_title, subject, duration, total_marks, start_date, end_date)
 VALUES 
-($teacher_id, '$exam_title', '$subject', $duration, $total_Questions, '$start_date', '$end_date')";
+($teacher_id, '$exam_title', '$subject', $duration, $total_marks, '$start_date', '$end_date')";
 
 if (mysqli_query($conn, $sql)) {
     echo json_encode([
