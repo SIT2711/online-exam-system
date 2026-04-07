f9025d10c9055f676d0c83cb9fe473b7ed116b18
student ofYashoda <jagtapsatyajit14@gmail.com>
2026-04-07 19:34:00 +0530

    Profile

---
+++ exam/delete_exam.php
@@ -0,0 +1,26 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+include "../config/db.php";
+
+$data = json_decode(file_get_contents("php://input"), true);
+$id = $data['id'];
+
+$sql = "DELETE FROM exams WHERE exam_id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id);
+
+if ($stmt->execute()) {
+    echo json_encode(["status" => "success"]);
+} else {
+    echo json_encode(["status" => "error"]);
+}
+?>
