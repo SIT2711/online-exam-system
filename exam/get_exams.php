73f4b416058a5ec5c7ee0de756d424104e73f409
Vasanti Dhankate <dr.dvasanti@gmail.com>
2026-04-08 12:39:07 +0530

    updated

--- exam/get_exams.php
+++ exam/get_exams.php
@@ -4,16 +4,22 @@ include "../config/db.php";
 header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
-// Fetch exams
-$sql = "SELECT exam_id, exam_title, subject, duration, total_marks, start_date, end_date FROM exams";
-$result = mysqli_query($conn, $sql);
+// Get the ID from the URL (React will send this)
+$teacher_id = isset($_GET['teacher_id']) ? mysqli_real_escape_string($conn, $_GET['teacher_id']) : '';
 
+if ($teacher_id) {
+    // Only get exams belonging to this teacher
+    $sql = "SELECT exam_id, exam_title FROM exams WHERE teacher_id = '$teacher_id'";
+} else {
+    echo json_encode(["status" => "error", "message" => "Missing teacher ID"]);
+    exit();
+}
+
+$result = mysqli_query($conn, $sql);
 $exams = [];
 
-if ($result) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $exams[] = $row;
-    }
+while ($row = mysqli_fetch_assoc($result)) {
+    $exams[] = $row;
 }
 
 echo json_encode([
